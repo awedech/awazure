94dbd3df3368cbdb870f1c0f867e3bec8692130c
Akash <awedech17366@gmail.com>
2021-10-07 13:15:51 +0200

    video fixxed

--- src/components/VideoPlayerProduct.vue
+++ src/components/VideoPlayerProduct.vue
@@ -1,6 +1,6 @@
 <template>
   <video id="vid1" class="azuremediaplayer amp-default-skin" autoplay controls width="640" height="400" data-setup='{"nativeControlsForTouch": false}'>
-        <source src="https://videoservice-euwe.streaming.media.azure.net/2c9d856d-4816-42a6-8cf2-3415c044afb8/Watch.ism/manifest" type="application/vnd.ms-sstr+xml" />
+        <source src="https://basicendpoint-videoservice-euwe.streaming.media.azure.net/2c9d856d-4816-42a6-8cf2-3415c044afb8/Watch.ism/manifest" type="application/vnd.ms-sstr+xml" />
         <p class="amp-no-js">
             To view this video please enable JavaScript, and consider upgrading to a web browser that supports HTML5 video
         </p>
